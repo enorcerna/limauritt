edba1a70d9725bc9306651d188d6c9facd73c611
Enor Cerna <u17200479@utp.edu.pe>
2024-06-15 04:09:30 -0500

    add title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,7 @@
 <svelte:head>
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
+	<title>App logo | Limauritt</title>
 </svelte:head>
 <div
 	class="bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)] w-screen max-h-screen fixed -z-10 top-0 bottom-0"
