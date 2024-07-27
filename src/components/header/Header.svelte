b832725745c53805452e3c3a4c1570e3c6b63f49
Enor Cerna <u17200479@utp.edu.pe>
2024-07-27 01:16:24 -0500

    add comp soft-icon

--- src/components/header/Header.svelte
+++ src/components/header/Header.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
 	import Responsive from '../global/Responsive.svelte';
 	import Dark from './Dark.svelte';
+	import Logo from './Logo.svelte';
+	import Software from './Software.svelte';
 </script>
 
 <header class="h-14 py-2 my-auto flex items-center z-10">
-	<Responsive className="flex items-center">
-		<div class="flex-1"></div>
-		<Dark />
+	<Responsive className="flex items-center justify-between">
+		<Logo />
+		<div class="flex gap-4 items-center">
+			<Software />
+			<Dark />
+		</div>
 	</Responsive>
 </header>
---
+++ src/components/header/Software.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Button from '../ui/button/button.svelte';
+</script>
+
+<Button class="gap-2 " variant="link" href="/proceso">
+	<img
+		src="https://inkscape.app/wp-content/uploads/inkscape-logo.webp"
+		alt="inkscape"
+		title="inkscape"
+		class="bg-cover w-full h-full"
+	/>
+	<span>Inkscape</span>
+</Button>
